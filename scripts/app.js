ef2a9908c65c407fa5d97f1a871f4b4540bc4bfc
Keenan <dancemanji@gmail.com>
2020-11-17 23:05:19 +0200

    second commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,21 @@
+window.onscroll = () => {myFunction();}
+
 function myFunction(){
-    var element = document.getElementById("Nav");
-    element.classList.add("navbar-scroll");
+    let element = document.getElementById("Nav");
+    let links = document.querySelectorAll(".nav-link");
+
+    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100)
+    {
+        element.classList.add("navbar-scroll");
+
+        for (let index = 0; index < links.length; index++) 
+            links[index].classList.add("nav-link-scroll");
+    }
+    else
+    {
+        element.classList.remove("navbar-scroll");
+
+        for (let index = 0; index < links.length; index++) 
+            links[index].classList.remove("nav-link-scroll");
+    }
 }
